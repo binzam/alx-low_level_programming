6119ab46c235b954c4e16f1c52a7dab488d6d5de
binyam <btechan@gmail.com>
2023-06-16 15:40:59 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print single digits from 0
+ * Return:return 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+{
+		printf("%d", i);
+}
+	printf("\n");
+	return (0);
+}
