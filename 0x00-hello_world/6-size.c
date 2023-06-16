7e22257941eede810ce8d1c9c5221828e1bee74a
binyam <btechan@gmail.com>
2023-06-15 17:00:24 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - A C program that prints the size of various types on the computer it is compiled and run on.
+ * main -program prints size of var types on the comp. it's compiled on
  * Return: 0 - Success
 */
 int main(void)
 {
 printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of long int: %zu byte(s)\n", sizeof(long));
 printf("Size of long long int: %zu byte(s)\n", sizeof(long long));
 printf("Size of float: %zu byte(s)\n", sizeof(float));
