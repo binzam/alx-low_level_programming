152599b5b3bd1fff9800d1c02a0518dd7c184c2b
binyam <btechan@gmail.com>
2023-06-16 17:03:05 -0700

    advanced task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print all combinaton of 2 digits.
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+}
+
+return (0);
+}
