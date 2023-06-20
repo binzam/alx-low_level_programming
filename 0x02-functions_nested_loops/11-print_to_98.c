86d2391a9dd5ebd63522f3e058a6dda72fcd37db
binyam <btechan@gmail.com>
2023-06-20 16:09:16 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,49 +6,13 @@
  */
 void print_to_98(int n)
 {
-if (n == 98)
-{
-_putchar('9');
-_putchar('8');
-_putchar('\n');
-return;
-}
-if (n > 98)
-{
-for (int i = n; i >= 98; i--)
-{
-int digit = i / 10;
-_putchar(digit + '0');
-digit = i % 10;
-_putchar(digit + '0');
-if (i != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-else
-{
-_putchar('\n');
-}
-}
-}
-else
-{
-for (int i = n; i <= 98; i++)
-{
-int digit = i / 10;
-_putchar(digit + '0');
-digit = i % 10;
-_putchar(digit + '0');
-if (i != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-else
-{
-_putchar('\n');
-}
-}
-}
+	int count;
+
+	if (n > 98)
+		for (count = n; count > 98; count--)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; count++)
+		printf("%d, ", count)
+			printf("98\n");
 }
