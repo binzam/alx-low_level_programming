ed9e7df6e959b22fe479e015cec59c82a5a5efb6
binyam <btechan@gmail.com>
2023-06-22 15:01:25 -0700

    task 6 7 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,9 +11,9 @@ void print_square(int size)
 {
 	int row, col;
 
-	for (row = 1; row <= size; row++)
+	for (row = 1; row < size; row++)
 	{
-		for (col = 1; col <= size; col++)
+		for (col = 1; col < size; col++)
 			_putchar('#');
 
 		_putchar('\n');
