4bd0733a091217c635d941a04f2679b540d18c8c
binyam <btechan@gmail.com>
2023-06-22 13:48:34 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 - 14 ,10 times
+ * followed by a new line
+ * Return: 0 (success)
+ */
+void more_numbers(void)
+{
+int line, numbers, num;
+
+for (line = 0; line < 11; line++)
+{
+for (numbers = 0; numbers < 15; numbers++)
+{
+num = numbers;
+
+if (numbers > 9)
+{
+_putchar(1 + '0');
+num = numbers % 10;
+}
+_putchar(num + '0');
+}
+_putchar('\n');
+}
+}
