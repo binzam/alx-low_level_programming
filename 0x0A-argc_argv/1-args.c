e100911e4a95950bf85da1111c1595ba3641e7e9
binyam <btechan@gmail.com>
2023-07-07 16:29:32 -0700

    task a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed into it
+ * @argc: int
+ * @argc: char
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
