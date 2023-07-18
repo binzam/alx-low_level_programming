84fa1af97ebd63216cad89be42102fe1e0a7f106
binyam <btechan@gmail.com>
2023-07-17 17:19:11 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: member 1 dogs name
+ * @age: member 2 dogs age
+ * @owner: member 3 owner name
+ *
+ * Description: defines a new data type that
+ *              can be used to represent dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
