5cb16fd28d241b76bfa75563f905be25ced09f54
binyam <btechan@gmail.com>
2023-07-27 17:17:11 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * startup - prints a message before the main function is executed
+ *
+ * Return: void
+ */
+void startup(void) __attribute__((constructor));
+
+void startup(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
