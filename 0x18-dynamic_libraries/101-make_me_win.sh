a3c28893b4e8410c23bfc06075b62ce637004a31
binyam <btechan@gmail.com>
2023-09-18 16:42:15 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/binzam/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgigaso
+export LD_PRELOAD=/tmp/libgiga.so
